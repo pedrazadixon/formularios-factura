a1657a7f8f91c55b862eac56abd83ab057c45c50
Dixon Pedraza <pedrazadixon@gmail.com>
2019-10-21 08:22:39 -0500

    base de datos y proyecto renombrado a formularios-factura

--- productos/crear.php
+++ productos/crear.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "formulario";
+    $dbname = "formularios-factura";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
