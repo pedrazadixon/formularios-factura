707a9f0e96f371ecb599d96517eb96834c671170
Estudiantes <estudiantes@CAMPUS.local>
2019-11-02 11:40:30 -0500

    actualizo db

--- formularios-factura.sql
+++ formularios-factura.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Oct 21, 2019 at 10:25 PM
--- Server version: 5.7.24
--- PHP Version: 7.2.19
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-11-2019 a las 17:40:05
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `formularios-factura`
+-- Base de datos: `formularios-factura`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clientes`
+-- Estructura de tabla para la tabla `clientes`
 --
 
 CREATE TABLE `clientes` (
@@ -34,10 +34,40 @@ CREATE TABLE `clientes` (
   `nit` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `razon_social`, `nit`) VALUES
+(1, 'SSEND.CO', '999999999'),
+(2, 'DAYAX.ML', '888888888');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `facturas`
+-- Estructura de tabla para la tabla `clientes_datos`
+--
+
+CREATE TABLE `clientes_datos` (
+  `id` int(11) NOT NULL,
+  `tipo` enum('EMAIL','TELEFONO','DIRECCION') NOT NULL,
+  `cliente_id` int(11) NOT NULL,
+  `dato` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `clientes_datos`
+--
+
+INSERT INTO `clientes_datos` (`id`, `tipo`, `cliente_id`, `dato`) VALUES
+(1, 'EMAIL', 1, 'admin@ssend.co'),
+(2, 'TELEFONO', 1, '7777777'),
+(3, 'DIRECCION', 1, 'cra 1 1 1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facturas`
 --
 
 CREATE TABLE `facturas` (
@@ -46,10 +76,17 @@ CREATE TABLE `facturas` (
   `fecha` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `facturas`
+--
+
+INSERT INTO `facturas` (`id_factura`, `cliente_id`, `fecha`) VALUES
+(1000, 1, '2019-11-02 17:10:36');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `facturas_productos`
+-- Estructura de tabla para la tabla `facturas_productos`
 --
 
 CREATE TABLE `facturas_productos` (
@@ -60,10 +97,17 @@ CREATE TABLE `facturas_productos` (
   `precio_factura` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `facturas_productos`
+--
+
+INSERT INTO `facturas_productos` (`id_facturas_productos`, `factura_id`, `producto_id`, `cantidad_factura`, `precio_factura`) VALUES
+(1, 1000, 1, 5, 5000);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `productos`
+-- Estructura de tabla para la tabla `productos`
 --
 
 CREATE TABLE `productos` (
@@ -74,60 +118,80 @@ CREATE TABLE `productos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_producto`, `descripcion`, `precio`, `cantidad`) VALUES
+(1, 'ESCOBA', 5000, 5),
+(2, 'TRAPERO', 8000, 3);
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `clientes`
+-- Indices de la tabla `clientes`
 --
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id_cliente`);
 
 --
--- Indexes for table `facturas`
+-- Indices de la tabla `clientes_datos`
+--
+ALTER TABLE `clientes_datos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `facturas`
 --
 ALTER TABLE `facturas`
   ADD PRIMARY KEY (`id_factura`);
 
 --
--- Indexes for table `facturas_productos`
+-- Indices de la tabla `facturas_productos`
 --
 ALTER TABLE `facturas_productos`
   ADD PRIMARY KEY (`id_facturas_productos`);
 
 --
--- Indexes for table `productos`
+-- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_producto`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `clientes`
+-- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `facturas`
+-- AUTO_INCREMENT de la tabla `clientes_datos`
+--
+ALTER TABLE `clientes_datos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `facturas`
 --
 ALTER TABLE `facturas`
-  MODIFY `id_factura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `id_factura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
 
 --
--- AUTO_INCREMENT for table `facturas_productos`
+-- AUTO_INCREMENT de la tabla `facturas_productos`
 --
 ALTER TABLE `facturas_productos`
-  MODIFY `id_facturas_productos` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_facturas_productos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `productos`
+-- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
